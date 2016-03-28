f23610d17f8f97141183ff78976d2604e67baf19
Gherbi Hicham <gherbixhicham@gmail.com>
2016-03-28 19:15:43 +0100

    connection method

--- src/Racinisation.java
+++ src/Racinisation.java
@@ -1,10 +1,56 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 // Cette class fait la racinisation manuelement 
 public class Racinisation {
+	private Connection con ;
+
+	private Statement stmt;
+
 	
 	public Racinisation (){
 		
 	}
+	
+    public void connectio ( String path ){
+		
+		
+		
+	    try {
+	        Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
 
+	        this.con = DriverManager.getConnection(path, "", "");
+
+	        this.stmt = con.createStatement();
+
+	        // Returns a ResultSet that contains the data produced by the query;
+	        // never null
+	        String query= "insert query here";
+	        ResultSet rs = stmt.executeQuery(query);
+
+
+	        while (rs.next()) {
+	            /*String fName = rs.getString("Field1");
+	           String lName = rs.getString("LastName");
+	            int age = rs.getInt("ID");*/
+	        }
+
+	        stmt.close();
+
+	        con.close();
+	    } catch (SQLException ex) {
+	        System.err.println("SQLException: " + ex.getMessage());
+	    } 
+	      catch (ClassNotFoundException e) {
+	    	  
+		    System.err.println("classnotfoundException: " + e.getMessage());
+
+	    	  
+	    }
+	}
 	
 	
 }
