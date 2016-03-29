aca6fc246c2fecf4b94ea1a0b4b3ad08a0d19a32
Gherbi Hicham <gherbixhicham@gmail.com>
2016-03-29 13:46:58 +0100

    gui for racinisation initial

---
+++ src/InterfaceRacinisaion.java
@@ -0,0 +1,26 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+//l'interface pour ajouté un racin pour chaque mot 
+
+public class InterfaceRacinisaion extends JFrame {
+	
+	private JButton Ok = new JButton();
+	
+	private JButton AjoutRacin = new JButton();
+	 
+	private JButton Annulé = new JButton();
+	
+	private JTextField TextMot = new JTextField();
+	
+	private JTextField TextRacin = new JTextField();
+	
+	public InterfaceRacinisaion(){
+		
+	}
+	
+	
+	
+
+}
