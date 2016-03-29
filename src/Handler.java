eebb133fe3728131f614ef6d57c619d0ccc9fb09
Gherbi Hicham <gherbixhicham@gmail.com>
2016-03-29 14:18:59 +0100

    adding new classes

---
+++ src/Handler.java
@@ -0,0 +1,31 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+//cette classe relie les donnée de l'interface et la classe Racnisation
+
+public class Handler {
+	
+	
+	Racinisation racinisation= new Racinisation();
+	
+	InterfaceRacinisaion interfaceRacin = new InterfaceRacinisaion();
+	
+	public Handler(Racinisation racinisation , InterfaceRacinisaion interfaceRacin){
+		
+		this.racinisation= racinisation;
+		this.interfaceRacin= interfaceRacin;
+		
+	    this.interfaceRacin.RacinInterfAddActionListener(new listnerPourInterface());
+	}
+	
+	class listnerPourInterface implements ActionListener{
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			
+			
+		}
+	}
+	
+	
+
+}
--- src/InterfaceRacinisaion.java
+++ src/InterfaceRacinisaion.java
@@ -1,3 +1,5 @@
+import java.awt.event.ActionListener;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextField;
@@ -19,6 +21,30 @@ public class InterfaceRacinisaion extends JFrame {
 	public InterfaceRacinisaion(){
 		
 	}
+	
+	//pour ajouté  l'actionListener dans la class Handler 
+	public void RacinInterfAddActionListener( ActionListener listner){
+		
+		this.AjoutRacin.addActionListener(listner);
+		this.Ok.addActionListener(listner);
+		this.Annulé.addActionListener(listner);
+		
+	}
+	
+	public JButton getOkButton (){
+		
+		return this.Ok;
+	}
+	
+	public JButton getAnnuleButton(){
+		return this.Annulé;
+	}
+	
+	public JButton getAjoutRacinButton(){
+		
+		return this.AjoutRacin;
+	}
+	
 	
 	
 	
