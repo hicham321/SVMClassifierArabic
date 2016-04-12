c354448bfff7e4fb3dd839ca5daf141ca87ef2bc
Gherbi Hicham <gherbixhicham@gmail.com>
2016-04-12 23:39:34 +0100

    positioning components on te panel

--- src/InterfaceRacinisaion.java
+++ src/InterfaceRacinisaion.java
@@ -1,7 +1,9 @@
 import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
@@ -11,13 +13,19 @@ public class InterfaceRacinisaion extends JFrame {
 	
 	private JButton Ok = new JButton("OK");
 	
-	private JButton AjoutRacin = new JButton("Ajouté un racine");
+	private JButton AjoutRacin = new JButton("Ajouté");
 	 
 	private JButton Annule = new JButton("Annulé");
 	
+	private JFileChooser Choisi;
+	
 	private JButton lanceRacinisation= new JButton("lance Racinisation");
 	
+	private JLabel MotLabel= new JLabel("Mot :");
+	
 	private JTextField TextMot = new JTextField(10);
+	
+	private JLabel RacinLabel = new JLabel("Racin :");
 	
 	private JTextField TextRacin = new JTextField(10);
 	
@@ -28,20 +36,42 @@ public class InterfaceRacinisaion extends JFrame {
         this.setSize(600, 300);
 		
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
-		
+				
 		JPanel panel = new JPanel();
+		
+		panel.setLayout(null);
 		
 		panel.add(this.Ok);
 		
+		this.Ok.setBounds(150, 220, 80, 25);
+				
 		panel.add(this.AjoutRacin);
 		
+		this.AjoutRacin.setBounds(350,110 , 100, 25);
+				
 		panel.add(this.Annule);
+		
+		this.Annule.setBounds(400, 220, 80, 25);
 		
 		panel.add(lanceRacinisation);
 		
+		this.lanceRacinisation.setBounds(350, 65, 100, 25);
+		
+		panel.add(this.MotLabel);
+		
+		this.MotLabel.setBounds(150, 65, 50, 25);
+		
 		panel.add(this.TextMot);
 		
+		this.TextMot.setBounds(200, 65, 100, 25);
+		
+		panel.add(this.RacinLabel);
+		
+		this.RacinLabel.setBounds(150, 110, 50, 25);
+		
 		panel.add(this.TextRacin);
+		
+		this.TextRacin.setBounds(200, 110, 100, 25);
 		
 		this.add(panel);
 
